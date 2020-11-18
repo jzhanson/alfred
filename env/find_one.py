e57e7671ddc3df5fe909e1d03715935e18761c4e
Joshua Zhanson <jzhanson@andrew.cmu.edu>
2020-11-18 12:29:35 -0500

    env/find_one.py: Refactor and add helper functions

--- env/find_one.py
+++ env/find_one.py
@@ -65,19 +65,92 @@ class FindOne(object):
         interactable_instance_ids = self.env.prune_by_any_interaction(
                 instance_ids)
         self.target_instance_id = random.choice(interactable_instance_ids)
-        for obj in event.metadata['objects']:
-            if obj['objectId'] == self.target_instance_id:
-                self.target_instance_index = self.obj_type_to_index[obj[
-                    'objectType']]
-                target_object = obj
+        target_object = event.get_object(self.target_instance_id)
+        self.target_instance_index = self.obj_type_to_index[target_object[
+            'objectType']]
 
         # Build scene graph
         self.graph = Graph(use_gt=True, construct_graph=True,
                 scene_id=scene_name_or_num)
 
         agent_height = event.metadata['agent']['position']['y']
-        # Find out which graph point is closest to the object is the closest
-        # and which way the agent should face
+        # Find out which graph point is closest to the object and which way the
+        # agent should face
+        self.end_pose, end_point_index = self.get_end_pose_point_index()
+
+        # Randomly initialize agent position
+        # len(self.graph.points) - 1 because randint is inclusive
+        start_point_index = random.randint(0, len(self.graph.points) - 1)
+        while start_point_index == end_point_index:
+            start_point_index = random.randint(0, len(self.graph.points) - 1)
+        start_point = self.graph.points[start_point_index]
+        start_pose = (start_point[0], start_point[1], random.randint(0, 3), 0)
+        action = {'action': 'TeleportFull',
+                  'x': start_pose[0] * constants.AGENT_STEP_SIZE,
+                  'y': agent_height,
+                  'z': start_pose[1] * constants.AGENT_STEP_SIZE,
+                  'rotateOnTeleport': True,
+                  'rotation': start_pose[2],
+                  'horizon': start_pose[3],
+                  }
+        event = self.env.step(action)
+
+        # Get "perfect" trajectory
+        self.update_expert_actions_path()
+        self.original_expert_actions = self.current_expert_actions
+        self.steps_taken = 0
+        self.done = False
+
+        return (event.frame, self.target_instance_index)
+
+    def step(self, action):
+        """
+        Figure out "correct" action via Graph, advance agent based on input
+        action, then return correct action along with obs, reward, done, info.
+        """
+        best_action = self.current_expert_actions[0]['action']
+
+        reward = self.rewards['step_penalty']
+        if self.done:
+            # Return None instead of last event and best action, and action
+            # unsuccessful
+            return (None, self.target_instance_index), reward, self.done, \
+                    (False, None, None)
+        if action == best_action:
+            reward = self.rewards['correct_action']
+
+        if action == ACTIONS_DONE or self.steps_taken == self.max_steps:
+            self.done = True
+            if action == ACTIONS_DONE and best_action == ACTIONS_DONE:
+                reward = self.rewards['success']
+            else:
+                reward = self.rewards['failure']
+
+            self.update_expert_actions_path()
+            # Currently, return None instead of the last event and action
+            # successful
+            return (None, self.target_instance_index), reward, self.done, \
+                    (True, None, best_action)
+
+        # Returns success, event, target_instance_id ('' if none),
+        # event.metadata['errorMessage'] ('' if none), api_action (action dict
+        # with forceAction and action)
+        success, event, _, _, _ = self.env.va_interact(action)
+
+        self.update_expert_actions_path()
+
+        self.steps_taken += 1
+
+        return (event.frame, self.target_instance_index), reward, self.done, \
+            (success, event, best_action) #obs, rew, done, info
+
+    def get_end_pose_point_index(self):
+        """
+        Get the end pose and the index of the ending point in the navigation
+        graph (self.graph) corresponding to self.target_instance_id
+        """
+        target_object = self.env.last_event.get_object(self.target_instance_id)
+        agent_height = self.env.last_event.metadata['agent']['position']['y']
         distances_to_target = []
         for point in self.graph.points:
             delta_x = target_object['position']['x'] - \
@@ -122,76 +195,23 @@ class FindOne(object):
             end_rotation = 3
         # TODO: figure up ending look up/down?
         #constants.VISIBILITY_DISTANCE
-        self.end_pose = (end_point[0], end_point[1], end_rotation, 0)
+        end_pose = (end_point[0], end_point[1], end_rotation, 0)
 
-        # Randomly initialize agent position
-        # len(self.graph.points) - 1 because randint is inclusive
-        start_point_index = random.randint(0, len(self.graph.points) - 1)
-        while start_point_index == end_point_index:
-            start_point_index = random.randint(0, len(self.graph.points) - 1)
-        start_point = self.graph.points[start_point_index]
-        start_pose = (start_point[0], start_point[1], random.randint(0, 3), 0)
-        action = {'action': 'TeleportFull',
-                  'x': start_pose[0] * constants.AGENT_STEP_SIZE,
-                  'y': agent_height,
-                  'z': start_pose[1] * constants.AGENT_STEP_SIZE,
-                  'rotateOnTeleport': True,
-                  'rotation': start_pose[2],
-                  'horizon': start_pose[3],
-                  }
-        event = self.env.step(action)
+        return end_pose, end_point_index
 
-        # Get "perfect" trajectory
-        expert_actions, expert_path = self.graph.get_shortest_path(
-                start_pose, self.end_pose)
-        # Add Done action to end of expert_actions
-        expert_actions.append({'action' : ACTIONS_DONE})
-        self.original_expert_actions = expert_actions
-        self.steps_taken = 0
-
-        self.current_expert_actions = expert_actions
-        self.current_expert_path = expert_path
-
-        return (event.frame, self.target_instance_index)
-
-    def step(self, action):
+    def update_expert_actions_path(self):
         """
-        Figure out "correct" action via Graph, advance agent based on input
-        action, then return correct action along with obs, reward, done, info.
+        Updates self.current_expert_actions and self.current_expert_path based
+        on current agent position.
         """
-        best_action = self.current_expert_actions[0]['action']
-
-        reward = self.rewards['step_penalty']
-        # TODO: reject action if already done
-        done = False
-        if action == best_action:
-            reward = self.rewards['correct_action']
-
-        if action == ACTIONS_DONE or self.steps_taken == self.max_steps:
-            done = True
-            if action == ACTIONS_DONE and best_action == ACTIONS_DONE:
-                reward = self.rewards['success']
-            else:
-                reward = self.rewards['failure']
-            # Currently, return None instead of the last event
-            return (None, self.target_instance_index), reward, done, (True,
-                    None, best_action)
-
-        # Returns success, event, target_instance_id ('' if none),
-        # event.metadata['errorMessage'] ('' if none), api_action (action dict
-        # with forceAction and action)
-        success, event, _, _, _ = self.env.va_interact(action)
-
         actions, path = self.graph.get_shortest_path(
                 self.env.last_event.pose_discrete, self.end_pose)
-        actions.append({'action' : ACTIONS_DONE})
+        # If there are no expert actions left and the environment is done, then
+        # ACTIONS_DONE was taken and should not be appended
+        if not (len(actions) == 0 and self.done):
+            actions.append({'action' : ACTIONS_DONE})
         self.current_expert_actions = actions
         self.current_expert_path = path
-
-        self.steps_taken += 1
-
-        return (event.frame, self.target_instance_index), reward, done, \
-            (success, event, best_action) #obs, rew, done, info
 
     # Some convenience functions to avoid poking around in FindOne internals
     def get_current_expert_actions_path(self):
