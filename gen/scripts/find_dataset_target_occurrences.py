dca5315aa8dc518e91a87dfc38f414eb5b427089
Joshua Zhanson <jzhanson@andrew.cmu.edu>
2021-02-08 15:41:28 -0500

    gen/scripts/find_dataset_target_occurrences.py: Script stuff, add output

--- gen/scripts/find_dataset_target_occurrences.py
+++ gen/scripts/find_dataset_target_occurrences.py
@@ -18,7 +18,6 @@ import pandas as pd
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 from tqdm import tqdm
 
@@ -44,11 +43,10 @@ with open('/home/jzhanson/alfred/data/find_one_find_pngs/valid_unseen.json', 'r'
 
 global current_trajectory_index
 current_trajectory_index = 0
-pbar = tqdm(total=len(train))
+pbar = tqdm(total=len(valid_unseen))
 
 lock = threading.Lock() # To protect current_trajectory_index
 
-# TODO: do this for valid_seen and valid_unseen
 def run(thread_id):
     thor_env = ThorEnv()
     fo = FindOne(thor_env, obj_type_to_index)
@@ -60,22 +58,22 @@ def run(thread_id):
     global current_trajectory_index
     while True:
         lock.acquire()
-        if current_trajectory_index < len(train):
+        if current_trajectory_index < len(valid_unseen):
             trajectory_index = current_trajectory_index
             current_trajectory_index += 1
             '''
-            print('Remaining trajectories: ' + str(len(train) -
+            print('Remaining trajectories: ' + str(len(valid_unseen) -
                 current_trajectory_index))
             '''
             pbar.update(1)
-            if current_trajectory_index == len(train):
+            if current_trajectory_index == len(valid_unseen):
                 pbar.close()
             lock.release()
         else:
             lock.release()
             break
 
-        trajectory = train[trajectory_index]
+        trajectory = valid_unseen[trajectory_index]
 
         # Load traj_data and see if any target is inside any other objects
         traj_data_path = os.path.join(trajectory['path'], 'traj_data.json')
@@ -113,10 +111,10 @@ def run(thread_id):
                             parent_receptacle['isOpen']))
 
     # Dump to JSON to sort later
-    with open('/home/jzhanson/alfred/data/train_target_occurrence_stats_' +
+    with open('/home/jzhanson/alfred/data/valid_unseen_hidden_target_stats_' +
             str(thread_id) + '.json', 'w+') as outfile:
         json.dump(targets_receptacles, outfile, indent=4)
-    with open('/home/jzhanson/alfred/data/train_target_occurrence_skipped_' +
+    with open('/home/jzhanson/alfred/data/valid_unseen_hidden_target_skipped_' +
             str(thread_id) + '.json', 'w+') as outfile:
         json.dump(skipped_trajectory_indexes, outfile, indent=4)
     print('Skipped trajectory indexes:')
@@ -136,7 +134,86 @@ for n in range(args.num_threads):
     time.sleep(1)
 
 '''
-counts = defaultdict(int)j
+total_hidden_targets = 0
+hidden_target_counts = defaultdict(int)
+containers_counts = defaultdict(int)
+combined_hidden_stats = []
+for i in range(8):
+    stats_path = '/home/jzhanson/alfred/data/valid_seen_hidden_target_stats_' + str(i) + '.json'
+    with open(stats_path, 'r') as jsonfile:
+        # List of trajectory_index, target_type, parent_receptacle_type,
+        # parent_receptacle is open
+        stats = json.load(jsonfile)
+        # Don't count toilets
+        current_hidden_targets = [traj for traj in stats if traj[2] != 'Toilet']
+        total_hidden_targets += len(current_hidden_targets)
+        for traj_index, target, receptacle, _ in stats:
+            hidden_target_counts[target] += 1
+            containers_counts[receptacle] += 1
+        combined_hidden_stats.extend(stats)
+
+print(len(combined_hidden_stats))
+with open('/home/jzhanson/alfred/data/find_one_find_pngs_valid_seen_hidden_object_stats.json', 'w') as outfile:
+    json.dump(combined_hidden_stats, outfile, indent=4)
+
+# 14K out of 100K training trajectories have a hidden target, not counting
+# toilets
+#
+# 541 out of valid_seen
+#
+# 707 out of valid_unseen
+#
+# train
+# [('Cup', 1509), ('Potato', 1495), ('Apple', 1247), ('Tomato', 1211), ('Mug',
+# 1038), ('Plate', 936), ('SoapBar', 894), ('Bowl', 719), ('SprayBottle', 571),
+# ('Candle', 569), ('SoapBottle', 542), ('ToiletPaper', 534), ('Cloth', 424),
+# ('DishSponge', 376), ('Egg', 339), ('Knife', 322), ('Lettuce', 306),
+# ('Ladle', 262), ('CD', 235), ('SaltShaker', 215), ('KeyChain', 201),
+# ('CreditCard', 195), ('Bread', 187), ('Pan', 181), ('PepperShaker', 172),
+# ('Spoon', 167), ('CellPhone', 153), ('ButterKnife', 149), ('Fork', 145),
+# ('Pencil', 124), ('Watch', 120), ('Pen', 117), ('Kettle', 111), ('Book',
+# 111), ('TissueBox', 105), ('Glassbottle', 91), ('Pot', 89), ('Spatula', 85),
+# ('WineBottle', 69), ('Vase', 39), ('HandTowel', 31), ('RemoteControl', 27),
+# ('Newspaper', 25), ('Box', 11)]
+#
+# [('Fridge', 5587), ('Cabinet', 4465), ('Drawer', 3007), ('Toilet', 2064),
+# ('Microwave', 1266), ('Safe', 60)]
+#
+# valid_seen
+# [('Apple', 85), ('Tomato', 77), ('Potato', 51), ('SoapBar', 42),
+# ('SprayBottle', 38), ('Plate', 36), ('ToiletPaper', 36), ('Cloth', 27),
+# ('Cup', 25), ('Lettuce', 21), ('ButterKnife', 18), ('Mug', 18), ('TissueBox',
+# 15), ('Knife', 15), ('CD', 15), ('CreditCard', 15), ('Bowl', 14),
+# ('DishSponge', 12), ('SoapBottle', 12), ('Egg', 9), ('KeyChain', 9),
+# ('Ladle', 8), ('Pot', 6), ('CellPhone', 6), ('RemoteControl', 5), ('Pan', 3),
+# ('Newspaper', 3), ('Fork', 3), ('Spatula', 3), ('Candle', 3), ('Pencil', 3)]
+#
+# [('Fridge', 235), ('Drawer', 141), ('Cabinet', 101), ('Toilet', 92),
+# ('Microwave', 61), ('Safe', 3)]
+#
+# valid_unseen
+# [('Mug', 93), ('Apple', 78), ('Cup', 71), ('SoapBar', 61), ('Plate', 45),
+# ('PepperShaker', 39), ('Lettuce', 36), ('Cloth', 36), ('CD', 30), ('Potato',
+# 30), ('KeyChain', 27), ('Vase', 21), ('ToiletPaper', 18), ('Egg', 18),
+# ('Bowl', 18), ('SaltShaker', 15), ('TissueBox', 15), ('Bread', 12),
+# ('Newspaper', 12), ('Tomato', 12), ('Glassbottle', 9), ('Pan', 9), ('Watch',
+# 9), ('Pencil', 9), ('CreditCard', 9), ('SoapBottle', 6), ('Box', 6),
+# ('ButterKnife', 3), ('CellPhone', 3), ('Knife', 3), ('Fork', 3)]
+#
+# [('Cabinet', 327), ('Fridge', 246), ('Drawer', 81), ('Toilet', 49), ('Safe',
+# 36), ('Microwave', 17)] 0%|
+print('total hidden targets: ' + str(total_hidden_targets))
+sorted_hidden_target_counts = sorted(hidden_target_counts.items(), key=lambda x:x[1], reverse=True)
+print('hidden target counts: ')
+print(sorted_hidden_target_counts)
+
+sorted_containers_counts = sorted(containers_counts.items(), key=lambda x:x[1], reverse=True)
+print('containers counts: ')
+print(sorted_containers_counts)
+'''
+
+'''
+counts = defaultdict(int)
 
 with open('/home/jzhanson/alfred/data/find_one_find_pngs/train.json', 'r') as jsonfile:
     train = json.load(jsonfile)
@@ -148,6 +225,7 @@ sorted_counts = sorted(counts.items(), key=lambda x:x[1], reverse=True)
 
 print('total trajectories: ' + str(len(train)))
 
+# 101295 training trajectories
 print('target counts: ')
 print(sorted_counts)
 '''
