ce65fa901c4f8fdfe7589c13be6eb6641af26500
Joshua Zhanson <jzhanson@andrew.cmu.edu>
2021-06-21 16:45:27 -0400

    gen/scripts/replay_trajectories.py: Add saving jpg/png and segs option

--- gen/scripts/replay_trajectories.py
+++ gen/scripts/replay_trajectories.py
@@ -156,12 +156,13 @@ if __name__ == '__main__':
         object_counts_visible = []
         object_counts_in_frame = []
         trajectory_replay_dict = {}
-        # Convert color_to_object_id to dict of have string keys since
-        # json.dump doesn't like tuples
-        color_to_object_id_str_keys = {}
-        for k, v in event.color_to_object_id.items():
-            color_to_object_id_str_keys[str(k)] = v
-        trajectory_replay_dict['color_to_object_id'] = color_to_object_id_str_keys
+        if args.save_segmentation:
+            # Convert color_to_object_id to dict of have string keys since
+            # json.dump doesn't like tuples
+            color_to_object_id_str_keys = {}
+            for k, v in event.color_to_object_id.items():
+                color_to_object_id_str_keys[str(k)] = v
+            trajectory_replay_dict['color_to_object_id'] = color_to_object_id_str_keys
         trajectory_start_time = time.time()
         print('trajectory %d/%d ' % (trajectory_number,
             len(trajectory_jsonfiles)))
@@ -169,17 +170,23 @@ if __name__ == '__main__':
                 trajectory_info['pred_action_indexes'],
                 trajectory_info['pred_mask_indexes']):
             # Save frame and segmentation
-            frame_save_path = os.path.join(replay_save_path, '%05d.png' % step)
+            if args.high_res_images:
+                image_extension = '.jpg'
+            else:
+                image_extension = '.png'
+            frame_save_path = os.path.join(replay_save_path, '%05d' % step +
+                    image_extension)
             cv2.imwrite(frame_save_path,
                     cv2.cvtColor(event.frame.astype('uint8'),
                         cv2.COLOR_RGB2BGR))
             # TODO: also save bounding boxes for each segmentation so we don't
             # have to compute it in data loader?
-            segmentation_save_path = os.path.join(replay_save_path,
-                    '%05d_segmentation.png' % step)
-            cv2.imwrite(segmentation_save_path,
-                    cv2.cvtColor(event.instance_segmentation_frame.astype('uint8'),
-                        cv2.COLOR_RGB2BGR))
+            if args.save_segmentation:
+                segmentation_save_path = os.path.join(replay_save_path,
+                        '%05d_segmentation' % step + image_extension)
+                cv2.imwrite(segmentation_save_path,
+                        cv2.cvtColor(event.instance_segmentation_frame
+                            .astype('uint8'), cv2.COLOR_RGB2BGR))
 
             # TODO: do we need to save superpixels? It would be faster but for
             # what purpose
