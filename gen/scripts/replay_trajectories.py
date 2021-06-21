234b8c7e5f8f58f73040f8823f53198c8167c4d3
Joshua Zhanson <jzhanson@andrew.cmu.edu>
2021-06-21 16:28:38 -0400

    gen/scripts/replay_trajectories.py: Add saving of frames, segs, labels

--- gen/scripts/replay_trajectories.py
+++ gen/scripts/replay_trajectories.py
@@ -20,6 +20,35 @@ from env.reward import InteractionReward
 from models.nn.ie import SuperpixelFusion
 import torch
 
+def get_object_counts_visible(event):
+    object_counts = [0 for _ in constants.ALL_OBJECTS]
+    objects = event.metadata['objects']
+    for obj in objects:
+        object_type = obj['objectType']
+        if obj['visible'] and object_type in constants.ALL_OBJECTS:
+            object_index = constants.ALL_OBJECTS.index(object_type)
+            object_counts[object_index] += 1
+    return object_counts
+
+def get_object_counts_in_frame(event):
+    object_ids_in_frame = set()
+    for i in range(event.instance_segmentation_frame.shape[0]):
+        for j in range(event.instance_segmentation_frame.shape[1]):
+            color = tuple(event.instance_segmentation_frame[i][j])
+            object_ids_in_frame.add(event.color_to_object_id[color])
+    object_counts = [0 for _ in constants.ALL_OBJECTS]
+    for object_id in object_ids_in_frame:
+        obj = event.get_object(object_id)
+        if obj is None:
+            # Some objects like "Cube", "Room", "Ceiling" aren't kept track of
+            # by the thor environment
+            continue
+        object_type = obj['objectType']
+        object_index = constants.ALL_OBJECTS.index(object_type)
+        object_counts[object_index] += 1
+    return object_counts
+
+# TODO: make this multiprocessing
 if __name__ == '__main__':
     """
     Requires load-path, single-interact, slic arguments, use-gt-segmentation
@@ -73,18 +102,39 @@ if __name__ == '__main__':
         num_actions = len(constants.COMPLEX_ACTIONS)
         index_to_action = constants.INDEX_TO_ACTION_COMPLEX
 
+    # TODO: access existing master index? Initialize master index? Do we need a
+    # master index?
+    if os.path.isdir(args.save_path):
+        pass
+    else:
+        os.makedirs(args.save_path)
+
     trajectory_jsonfiles = [fname for fname in os.listdir(args.load_path)
             if 'json' in fname]
     trajectory_jsonfiles.sort()
     #trajectory_jsonfiles = ['0.json']
+    start_time = time.time()
+    total_steps = 0
     for trajectory_info_name in trajectory_jsonfiles:
         jsonfile_full_path = os.path.join(args.load_path, trajectory_info_name)
-
         with open(jsonfile_full_path, 'r') as jsonfile:
             trajectory_info = json.load(jsonfile)
 
-        print(trajectory_info_name)
-        print(trajectory_info.keys())
+        trajectory_number = int(trajectory_info_name.split('.')[0])
+        replay_save_path = os.path.join(args.save_path, str(trajectory_number))
+        # Trajectory is already replayed (in a previous script run) if
+        # trajectory_replay_dict is saved
+        trajectory_replay_dict_save_path = os.path.join(replay_save_path,
+                'info.json')
+        if os.path.isdir(replay_save_path):
+            if os.path.isfile(trajectory_replay_dict_save_path):
+                continue
+            # If directory exists but trajectory_replay_dict was not saved to
+            # file (i.e. trajectory not completely replayed), no need to do
+            # anything
+        else:
+            os.mkdir(replay_save_path)
+
         frame = ie.reset(trajectory_info['scene_num'])
         event = ie.get_last_event()
         thor_env.restore_scene(trajectory_info['object_poses'], [], False)
@@ -102,9 +152,39 @@ if __name__ == '__main__':
                   }
         event = thor_env.step(init_pose_action)
 
+        step = 0
+        object_counts_visible = []
+        object_counts_in_frame = []
+        trajectory_replay_dict = {}
+        # Convert color_to_object_id to dict of have string keys since
+        # json.dump doesn't like tuples
+        color_to_object_id_str_keys = {}
+        for k, v in event.color_to_object_id.items():
+            color_to_object_id_str_keys[str(k)] = v
+        trajectory_replay_dict['color_to_object_id'] = color_to_object_id_str_keys
+        trajectory_start_time = time.time()
+        print('trajectory %d/%d ' % (trajectory_number,
+            len(trajectory_jsonfiles)))
         for pred_action_index, pred_mask_index in zip(
                 trajectory_info['pred_action_indexes'],
                 trajectory_info['pred_mask_indexes']):
+            # Save frame and segmentation
+            frame_save_path = os.path.join(replay_save_path, '%05d.png' % step)
+            cv2.imwrite(frame_save_path,
+                    cv2.cvtColor(event.frame.astype('uint8'),
+                        cv2.COLOR_RGB2BGR))
+            # TODO: also save bounding boxes for each segmentation so we don't
+            # have to compute it in data loader?
+            segmentation_save_path = os.path.join(replay_save_path,
+                    '%05d_segmentation.png' % step)
+            cv2.imwrite(segmentation_save_path,
+                    cv2.cvtColor(event.instance_segmentation_frame.astype('uint8'),
+                        cv2.COLOR_RGB2BGR))
+
+            # TODO: do we need to save superpixels? It would be faster but for
+            # what purpose
+            object_counts_visible.append(get_object_counts_visible(event))
+            object_counts_in_frame.append(get_object_counts_in_frame(event))
             # Make frame torch and (3, 300, 300) to match with code in
             # get_superpixel_masks_frame_crops or
             # get_gt_segmentation_masks_frame_crops
@@ -117,18 +197,45 @@ if __name__ == '__main__':
                 # doesn't care about color_to_object_id, the results will be
                 # the same even if instance_segmentation_frame has different
                 # colors.
-                masks, frame_crops = sf.get_gt_segmentation_masks_frame_crops(
-                        torch.from_numpy(np.ascontiguousarray(event.frame).transpose(2,
-                            0, 1)), event.instance_segmentation_frame)
+                masks, frame_crops = (SuperpixelFusion
+                        .get_gt_segmentation_masks_frame_crops(
+                            torch.from_numpy(np.ascontiguousarray(event.frame)
+                                .transpose(2, 0, 1)),
+                            event.instance_segmentation_frame,
+                            boundary_pixels=args.boundary_pixels,
+                            black_outer=args.black_outer))
             else:
-                masks, frame_crops = sf.get_superpixel_masks_frame_crops(
-                        torch.from_numpy(np.ascontiguousarray(event.frame).transpose(2, 0, 1)))
+                masks, frame_crops = (SuperpixelFusion
+                        .get_superpixel_masks_frame_crops(
+                            torch.from_numpy(np.ascontiguousarray(event.frame)
+                                .transpose(2, 0, 1)), slic_kwargs=slic_kwargs,
+                            boundary_pixels=args.boundary_pixels,
+                            neighbor_depth=args.neighbor_depth,
+                            neighbor_connectivity=args.neighbor_connectivity,
+                            black_outer=args.black_outer))
 
             selected_action = index_to_action[pred_action_index]
+            print('masks, pred_mask_index', len(masks), pred_mask_index)
             selected_mask = (masks[pred_mask_index] if pred_mask_index >= 0
                     else None)
             _, reward, _, (action_success, event, err) = ie.step(
                     selected_action,
                     interact_mask=selected_mask)
             print(selected_action, action_success, reward, err)
+            step += 1
+        # object_counts_visible can't be computed post-hoc because we don't
+        # have the environment to figure out whether objects are within
+        # VISIBILITY_DISTANCE, but object_counts_in_frame can - we still
+        # include it for faster dataset loading
+        trajectory_replay_dict['object_counts_visible'] = object_counts_visible
+        trajectory_replay_dict['object_counts_in_frame'] = (
+                object_counts_in_frame)
+        with open(trajectory_replay_dict_save_path, 'w') as jsonfile:
+            json.dump(trajectory_replay_dict, jsonfile)
 
+        total_steps += step
+        current_time = time.time()
+        total_fps = total_steps / (current_time - start_time)
+        trajectory_fps = step / (current_time - trajectory_start_time)
+        print('total fps since start %.6f' % total_fps)
+        print('trajectory fps %.6f' % trajectory_fps)
